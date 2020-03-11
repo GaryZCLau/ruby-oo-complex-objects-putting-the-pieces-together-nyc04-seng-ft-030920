41c9800d06b4bfb67fc29fc5ac1289998d7c81bb
Gary Lau <gareeeliu@gmail.com>
2020-03-11 15:21:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
 # Make your shoe class here!
 class Shoe
   
-  attr_accessible :brand, :color, :size, :material, :condition
+  attr_accessible :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialized(brand)
-    
+    @brand = brand
   end
   
 end
